8319df0250fcd0fd96a4699a8cdb3f01223403d7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-21 15:36:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import { View, Text, Button, Image } from 'react-native'
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+
 
 const SignIn = ({ navigation }) => {
   return (
-    <View className='bg-purple-700/40 h-full'>
-
-    </View>
+    <LinearGradient
+      colors={['#af6df9', '#FFFFFF']}
+      style={styles.gradient}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 0, y: 0 }}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}> Gradient Background</Text>
+      </View>
+    </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+    gradient: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    container: {
+      // Additional styling for your container
+    },
+    text: {
+      color: '#fff',
+      fontSize: 20,
+    },
+  });
 
 export default SignIn
