87dca9bef975d46a15fd1b0c3e7183339dad2597
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-21 16:01:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text , Image} from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
 
 
 const SignIn = ({ navigation }) => {
   return (
     <LinearGradient
-      colors={['#af6df9', '#FFFFFF']}
+      colors={['#cda4fb', '#e3ccfc']}
       style={styles.gradient}
       start={{ x: 0, y: 1 }}
       end={{ x: 0, y: 0 }}
     >
       <View style={styles.container}>
-        <Text style={styles.text}> Gradient Background</Text>
+        <Text className="text-center font-semibold text-xl text-gray-600">Welcome Back</Text>
+
+        <Image
+            source={require('../assets/PImage3.png')} 
+            style={{
+                width: 300,
+                height: 300,
+                resizeMode: 'contain',
+              }}
+        >            
+        </Image>
+
+        <Text className="text-center font-semibold text-xl text-gray-600">login Here</Text>
+
+
+        <Text className="text-center text-gray-600">Don't have an Account ? <Text className="text-purple-800 font-semibold" onPress={() => navigation.navigate('SignUp')}>Register</Text></Text>
       </View>
     </LinearGradient>
   )
@@ -21,14 +36,14 @@ const SignIn = ({ navigation }) => {
 const styles = StyleSheet.create({
     gradient: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      paddingTop: 80,
+      alignItems: 'center'
     },
     container: {
       // Additional styling for your container
     },
     text: {
-      color: '#fff',
+      color: '#000',
       fontSize: 20,
     },
   });
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,21 +1,52 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import React from 'react';
+import { View, StyleSheet, Text , Image} from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+
 
 const SignUp = ({ navigation }) => {
   return (
-    <View>
-        <Text className="text-4xl text-red-500 bg-green-500">
-            SignUp
-        </Text>
-        <Button
-            className="text-blue-900"
-            title='SignUp'
-            onPress={() => navigation.navigate('SignIn')}
-        >
+    <LinearGradient
+      colors={['#FAC109', '#ffff90']}
+      style={styles.gradient}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 0, y: 0 }}
+    >
+      <View style={styles.container}>
+        <Text className="text-center font-semibold text-xl text-gray-600">Welcome to</Text>
+        <Text className="text-center font-semibold text-gray-600">E-Library</Text>
 
-        </Button>
-    </View>
+        <Image
+            source={require('../assets/pImage1.png')} 
+            style={{
+                width: 300,
+                height: 300,
+                resizeMode: 'contain',
+              }}
+        >            
+        </Image>
+
+        <Text className="text-center font-semibold text-xl text-gray-600">SignUp Here</Text>
+
+
+        <Text className="text-center text-gray-600">Already have an Account ? <Text className="text-purple-800 font-semibold" onPress={() => navigation.navigate('SignIn')}>SignIn</Text></Text>
+      </View>
+    </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+    gradient: {
+      flex: 1,
+      paddingTop: 80,
+      alignItems: 'center'
+    },
+    container: {
+      // Additional styling for your container
+    },
+    text: {
+      color: '#000',
+      fontSize: 20,
+    },
+  });
 
 export default SignUp
