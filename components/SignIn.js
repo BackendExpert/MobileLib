270c079bc8d331b9c004150f084c8a362c334db6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-21 15:17:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,8 +3,8 @@ import { View, Text, Button, Image } from 'react-native'
 
 const SignIn = ({ navigation }) => {
   return (
-    <View>
-        
+    <View className='bg-purple-700/40 h-full'>
+
     </View>
   )
 }
